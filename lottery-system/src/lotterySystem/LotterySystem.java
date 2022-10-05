a7ef3fd33df8df0c7150f53c428b59515412b596
Eleanor CM <97481864+EleanorCM@users.noreply.github.com>
2022-10-05 01:33:51 +0100

    added folders

---
+++ lottery-system/src/lotterySystem/LotterySystem.java
@@ -0,0 +1,13 @@
+package lotterySystem;
+
+public class LotterySystem {
+
+		/* Scenario : you have been added to a development team creating a lottery system (currently paper based). The system will be developed incrementally, based on a series of user requests (User Stories).  A user story is a general explanation of a software feature written from the perspective of the user: a user in this case could be the Lottery Player, the owner of the Lottery System or even a member of the Development Team. The system will be developed in an Agile format with a series of Sprints (a time-limited development and testing period with some User Stories to be completed in that Sprint). Each Sprint will build on the previous Sprint's code base. You'll also have some instructions and technical Hints from a Product Owner (part of the Agile Team).
+
+Note : as you develop your skills throughout the course you'll be able to upgrade the code as appropriate with new constructs! We'll manage that as we go through the project. Enjoy. */
+	
+	public static void main(String[] args) {
+		
+	}
+
+}
